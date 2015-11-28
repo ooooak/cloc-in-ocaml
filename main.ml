ed572067fa9ab3702ac748d3115c955397c88b2e
Ak <akshay.deep0@gmail.com>
2015-11-28 15:53:07 +0530

    Can search all files from given dir

--- main.ml
+++ main.ml
@@ -1,53 +1,28 @@
-open Printf
+let print = Printf.printf
+let loga = Array.iter (print "%s\n") 
+let logl = List.iter (print "%s\n")
 
-let is_dir name = 
-    try 
-        Sys.is_directory name
-    with 
-        Sys_error _ -> false
+let print = Printf.printf
+let is_dir name = try match Sys.is_directory name with
+    | true -> `True
+    | false -> `False
+    with Sys_error _ -> `Error
 
-let absp path_name = Filename.current_dir_name ^ ""
+let files dir = dir |> Sys.readdir |> Array.to_list
 
-let rec collect lists files dirs = match lists with 
-    | [] -> (files, dirs)
-    | h :: t -> (match is_dir h with
-       | true   -> collect t files (h :: dirs)
-       | false  -> collect t (h :: files) dirs)
-;;
+(* should i just return list then append it ? *)
+(* this function can be improved *)
+let (/) = Filename.concat
+let rec scan start storage = match start with
+    | "" -> !storage
+    | _ -> files start |> List.iter (fun f -> 
+        let current = (start / f) in 
+        match is_dir current  with
+        | `True -> let _ = scan current storage in ()
+        | `False -> storage := current :: !storage; ()
+        | `Error -> ()
+    );
+    !storage
 
-let make_path dir name = dir ^ "\\" ^ name
-
-(*  file1 
-    file2
-    dir1
-        file1
-        dir1
-            dir1
-                dir1
-                    file
-    file3
-    dir2
-*)
-
-let scan dir = 
-    let current_list = dir |> Sys.readdir 
-                           |> Array.map (make_path dir)
-                           |> Array.to_list in 
-    match collect current_list [] [] with
-    | files, [] -> files
-    | _, dirs ->  []
-;;
-
-
-
-let main =
-    match Array.length Sys.argv with
-    | 0 | 1 -> printf("Error: Dir name is missing.")
-    | _ -> 
-        let dir_name = Sys.argv.(1) in 
-        if is_dir dir_name then
-            scan dir_name
-        else
-            printf("Error: Invalid directory name.")
-
-
+let main = 
+  scan "test" (ref []) |> logl
--- play.ml
+++
@@ -1,9 +0,0 @@
-let l = ["test";"tw"]
-
-
-let () =
-    print_string (make_path "test" "test")
-
-(*     let v = List.iter (make_path "test") l in
-      List.iter print_string v; () *)
-  ;; 
